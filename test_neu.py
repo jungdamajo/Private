c0f0b5718f7c2df9d5f272b3ecc99814f2d72c98
jungdamajo <jungda01@students.zhaw.ch>
2021-03-28 18:56:48 +0200

    New

--- test_neu.py
+++ test_neu.py
@@ -1,2 +1,8 @@
-python3 -m venv .venv
-source .venv/bin/activate
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
+plt.plot(x, np.sin(x))       # Plot the sine of each x point
+plt.show()                   # Display the plot
